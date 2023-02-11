78211a1c6d160ab0597179938ee4aed23dacd2fd
alevidals <alejandro.vida.sanchez16@gmail.com>
2023-02-12 00:19:28 +0100

    feat: create base components and add prettierc

---
+++ app/components/TodoTable.tsx
@@ -0,0 +1,11 @@
+export function TodoTable() {
+  return (
+    <div className="h-[450px] rounded-md bg-gray-100 p-10 shadow-2xl">
+      <h2 className="text-3xl font-semibold text-slate-800">Reminders</h2>
+    </div>
+    // <div className="bg-green-500 h-[450px] rounded-md shadow-2xl grid grid-cols-4 overflow-hidden">
+    //   <div className="bg-blue-400"></div>
+    //   <div className="bg-red-400 col-span-3"></div>
+    // </div>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,10 @@
+import { TodoTable } from "~/components/TodoTable";
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main className="container mx-auto mt-10">
+      <h1 className="mb-10 text-3xl font-bold text-slate-800">Todo</h1>
+      <TodoTable />
+    </main>
   );
 }
