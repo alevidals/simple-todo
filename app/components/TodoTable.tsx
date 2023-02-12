428d9d5c5000d61f3246aa77916149ec128de8cd
alevidals <alejandro.vida.sanchez16@gmail.com>
2023-02-12 15:29:57 +0100

    feat: add the function to add todos

--- app/components/TodoTable.tsx
+++ app/components/TodoTable.tsx
@@ -1,5 +1,7 @@
 import { PlusIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import { Form } from "@remix-run/react";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 type Todo = {
   id: string;
@@ -15,11 +17,24 @@ function classNames(...classes: string[]) {
 }
 
 export function TodoTable({ todos }: TodoTableProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   return (
     <div className="h-[450px] overflow-y-scroll rounded-md bg-gray-100 p-10 shadow-2xl">
       <header className="flex items-center justify-between">
         <h2 className="text-3xl font-semibold text-slate-800">Reminders</h2>
-        <button className="h-5 w-5 text-slate-800 transition duration-100 hover:text-slate-500">
+        <button
+          className="h-5 w-5 text-slate-800 transition duration-100 hover:text-slate-500"
+          onClick={openModal}
+        >
           <PlusIcon />
         </button>
       </header>
@@ -45,6 +60,62 @@ export function TodoTable({ todos }: TodoTableProps) {
           </li>
         ))}
       </ul>
+      {/*  */}
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-40" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Form method="post">
+                    <div className="mb-5 flex flex-col space-y-2">
+                      <label
+                        htmlFor="todo"
+                        className="select-none font-semibold text-slate-800"
+                      >
+                        Todo
+                      </label>
+                      <input
+                        type="text"
+                        name="todo-text"
+                        id="todo"
+                        autoComplete="off"
+                        className="h-11 rounded-md bg-gray-200 px-3 text-slate-800 accent-slate-800"
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      className="h-11 w-full rounded-lg bg-slate-800 px-4 font-bold text-white transition duration-100 hover:bg-slate-900"
+                    >
+                      Submit
+                    </button>
+                  </Form>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
     // <div className="bg-green-500 h-[450px] rounded-md shadow-2xl grid grid-cols-4 overflow-hidden">
     //   <div className="bg-blue-400"></div>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,44 +5,89 @@ import { useLoaderData } from "@remix-run/react";
 import { Header } from "~/components/Header";
 import { TodoTable } from "~/components/TodoTable";
 import { db } from "~/utils/db.server";
-import { getUser, getUserId, requireUserId } from "~/utils/session.server";
+import { badRequest } from "~/utils/request.server";
+import { getUser, requireUserId } from "~/utils/session.server";
+
+function validateTodoText(todoText: string) {
+  if (!validateTodoText) {
+    return "The text can not be empty";
+  }
+}
 
 export const action = async ({ request }: LoaderArgs) => {
   const form = await request.formData();
+  const userId = await requireUserId(request);
 
-  if (request.method === "DELETE") {
-    const todoId = form.get("id");
+  switch (request.method) {
+    case "DELETE": {
+      const todoId = form.get("id");
 
-    if (!todoId || typeof todoId !== "string") {
-      throw new Response("There was a problem trying to delete the todo", {
-        status: 400,
+      if (typeof todoId !== "string") {
+        throw new Response("There was a problem trying to delete the todo", {
+          status: 400,
+        });
+      }
+
+      const todo = await db.todo.findUnique({
+        where: { id: todoId },
+      });
+
+      if (!todo) {
+        throw new Response("Can't delete what does not exist", {
+          status: 404,
+        });
+      }
+
+      if (todo.userId !== userId) {
+        throw new Response("Pssh, nice try. That's not your todo", {
+          status: 403,
+        });
+      }
+
+      await db.todo.delete({ where: { id: todoId } });
+
+      return redirect("/");
+    }
+    case "POST": {
+      const todoText = form.get("todo-text");
+
+      if (typeof todoText !== "string") {
+        return badRequest({
+          fieldErrors: null,
+          fields: null,
+          formError: `Form not submitted correctly.`,
+        });
+      }
+
+      const fieldErrors = {
+        todoText: validateTodoText(todoText),
+      };
+
+      const fields = { todoText };
+
+      if (Object.values(fieldErrors).some(Boolean)) {
+        return badRequest({
+          fieldErrors,
+          fields,
+          formError: null,
+        });
+      }
+
+      await db.todo.create({
+        data: {
+          text: todoText,
+          userId: userId,
+        },
+      });
+
+      return redirect("/");
+    }
+    default: {
+      throw new Response("Method not allowed", {
+        status: 405,
       });
     }
-
-    const todo = await db.todo.findUnique({
-      where: { id: todoId },
-    });
-
-    if (!todo) {
-      throw new Response("Can't delete what does not exist", {
-        status: 404,
-      });
-    }
-
-    const userId = await getUserId(request);
-
-    if (todo.userId !== userId) {
-      throw new Response("Pssh, nice try. That's not your todo", {
-        status: 403,
-      });
-    }
-
-    await db.todo.delete({ where: { id: todoId } });
-
-    return redirect("/");
   }
-
-  return redirect("/");
 };
 
 export const loader = async ({ request }: LoaderArgs) => {
